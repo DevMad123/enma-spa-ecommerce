363fa76b8d091bfe168edc2f231ff7a641266a55
DevMad123 <ngolomadououattara@gmail.com>
2025-10-15 20:27:07 +0000

    Last Conf

---
+++ config/database.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
+        // tu peux aussi laisser mysql ou sqlite si tu en as besoin
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_MYSQL', '127.0.0.1'),
+            'port' => env('DB_PORT_MYSQL', '3306'),
+            'database' => env('DB_DATABASE_MYSQL', 'forge'),
+            'username' => env('DB_USERNAME_MYSQL', 'forge'),
+            'password' => env('DB_PASSWORD_MYSQL', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+
+    'migrations' => 'migrations',
+
+];
