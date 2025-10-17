5c6111c02ce71fddab439228a06bbd4d47151145
Ngolo OUATTARA <ngolo@emiciv.fr>
2025-10-17 09:57:03 +0000

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    build: {
-        rollupOptions: {
-            external: [
-                '@testing-library/react',
-                '@testing-library/jest-dom'
-            ]
+export default defineConfig(({ mode }) => {
+    // Charger les variables d’environnement (.env)
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: '/build/', // ✅ IMPORTANT pour Render
+        plugins: [
+            laravel({
+                input: ['resources/js/app.jsx'],
+                refresh: true,
+            }),
+            react(),
+        ],
+        build: {
+            outDir: 'public/build',
+            manifest: true,
+            rollupOptions: {
+                external: [
+                    '@testing-library/react',
+                    '@testing-library/jest-dom'
+                ]
+            }
         }
-    }
+    };
 });
